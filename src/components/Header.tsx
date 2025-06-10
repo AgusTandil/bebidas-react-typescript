9c6b34a0a975b3005d6f58c421bd7ad5df174a99
amradeland <amradeland@gylgroup.com>
2025-06-10 16:01:07 -0300

    zustand slice, zos schema

---
+++ src/components/Header.tsx
@@ -0,0 +1,91 @@
+import { useEffect, useMemo } from "react";
+import { NavLink, useLocation } from "react-router-dom";
+import { useAppStore } from "../stores/useAppStore";
+// NAVlINK VIENE CON UN CALLBACK QUE NOS PERMITE DARLE DISTINTOS ESTILOS A LINK DEPENDIENDO DE DONDE ESTEMOS PARADOS. SI ESTAMOS EN INICIO LA PALABRA INICIO SE PONE NARANJA POR EJ.
+
+export default function Header() {
+  const { pathname } = useLocation();
+
+  // Esto se ejecuta cada vez que el pathname cambie.
+  const isHome = useMemo(() => pathname === "/", [pathname]);
+  const { fetchCategories } = useAppStore();
+  useEffect(() => {
+    fetchCategories()
+  }, []);
+  return (
+    <header
+      className={
+        isHome ? "bg-[url('/bg.jpg')] bg-center bg-cover" : "bg-slate-800"
+      }
+    >
+      <div className="mx-auto container px-5 py-16">
+        <div className="flex justify-between items-center">
+          <div>
+            <img src="/logo.svg" alt="logotipo" className="w-32" />
+          </div>
+          <nav className="flex gap-4">
+            <NavLink
+              to="/"
+              className={({ isActive }) =>
+                isActive
+                  ? "text-orange-500 uppercase font-bold"
+                  : "text-white uppercase font-bold"
+              }
+            >
+              Inicio
+            </NavLink>
+            <NavLink
+              to="/favoritos"
+              className={({ isActive }) =>
+                isActive
+                  ? "text-orange-500 uppercase font-bold"
+                  : "text-white uppercase font-bold"
+              }
+            >
+              Favoritos
+            </NavLink>
+          </nav>
+        </div>
+        {isHome && (
+          <form className="md:w-1/2 2xl:w-1/3 bg-orange-400 my-32 p-10 rounded-lg shadow space-y-6">
+            <div className="space-y-4 ">
+              <label
+                htmlFor="ingredient"
+                className="block text-white uppercase font-extrabold text-lg"
+              >
+                Nombre o Ingredientes
+              </label>
+              <input
+                type="text"
+                className="p-3 w-full rounded-lg focus:outline-none bg-slate-200"
+                id="ingredient"
+                name="ingredient"
+                placeholder="Nombre o Ingrediente. Ej vodka, tequila, wisky"
+              />
+            </div>
+            <div className="space-y-4 ">
+              <label
+                htmlFor="ingredient"
+                className="block text-white uppercase font-extrabold text-lg"
+              >
+                Categoría
+              </label>
+              <select
+                className="p-3 w-full rounded-lg focus:outline-none bg-slate-200"
+                id="ingredient"
+                name="ingredient"
+              >
+                <option>-- Seleccione --</option>
+              </select>
+            </div>
+            <input
+              type="submit"
+              value="Buscar recetas"
+              className="cursor-pointer bg-orange-800 hover:bg-orange-900 text-white font-extrabold w-full p-2 rounded-lg uppercase"
+            />
+          </form>
+        )}
+      </div>
+    </header>
+  );
+}
---
+++ src/views/IndexPage.tsx
@@ -0,0 +1,10 @@
+import { useAppStore } from "../stores/useAppStore"
+export default function IndexPage() {
+
+  const {categories} = useAppStore();
+  return (
+    <>
+   <h1>Inicio</h1>
+    </>
+  )
+}
